0aad85e56656ccea9627395bb505820a58289a7a
jaws <order18291@gmail.com>
2021-09-30 13:04:19 +0900

    로그인뺴고_다함

--- board/forms.py
+++ board/forms.py
@@ -6,4 +6,10 @@ class AnswerForm(forms.ModelForm):
     model = Answer
     fields = ['content']
     labels = {'content':'내용'}
+
+class QuestionForm(forms.ModelForm):
+  class Meta:
+    model = Question
+    fields = ['subject','content']
+    labels = {'subject':'제목','content':'내용'}
     
--- board/views.py
+++ board/views.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from board.models import Question,Answer
-from board.forms import AnswerForm
+from board.forms import AnswerForm, QuestionForm
 # Create your views here.
 
 def index(request):
@@ -29,3 +29,16 @@ def answer_create(request,question_id):
     form = AnswerForm()
   context = {'form':form, 'question':question}
   return render(request,'board/detail.html',context)
+
+def question_create(request):
+  if request.method == "POST":
+    form = QuestionForm(request.POST)
+    if form.is_valid():
+      question = form.save(commit=False)
+      question.create_date = timezone.now()
+      question.save()
+      return redirect('board:index')
+  else:
+    form = QuestionForm()
+  context = {'form':form}
+  return render(request,'board/question_create.html',context)
