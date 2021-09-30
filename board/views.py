c5619e67567a5f14f147ae3a90faaead059b62aa
jaws <order18291@gmail.com>
2021-09-30 16:12:15 +0900

    글쓴이표시완료

--- board/views.py
+++ board/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from board.models import Question,Answer
 from board.forms import AnswerForm, QuestionForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -19,6 +20,7 @@ def detail(request,question_id):
   context = {"question":question}
   return render(request,'board/detail.html',context)
 
+@login_required(login_url='common:login')
 def answer_create(request,question_id):
   question = get_object_or_404(Question, pk=question_id)
   if request.method == "POST":
@@ -27,6 +29,7 @@ def answer_create(request,question_id):
       answer = form.save(commit=False)
       answer.question = question
       answer.create_date = timezone.now()
+      answer.author = request.user
       answer.save()
       return redirect('board:detail', question_id=question.id)
   else:
@@ -34,12 +37,14 @@ def answer_create(request,question_id):
   context = {'form':form, 'question':question}
   return render(request,'board/detail.html',context)
 
+@login_required(login_url='common:login')
 def question_create(request):
   if request.method == "POST":
     form = QuestionForm(request.POST)
     if form.is_valid():
       question = form.save(commit=False)
       question.create_date = timezone.now()
+      question.author = request.user
       question.save()
       return redirect('board:index')
   else:
