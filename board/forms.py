768db124dec8c49444718392dcac6d8c8f673b5b
jaws <order18291@gmail.com>
2021-10-01 10:36:31 +0900

    댓글모두완료

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from board.models import Question,Answer
+from board.models import Question,Answer,Comment
 
 class AnswerForm(forms.ModelForm):
   class Meta:
@@ -12,4 +12,10 @@ class QuestionForm(forms.ModelForm):
     model = Question
     fields = ['subject','content']
     labels = {'subject':'제목','content':'내용'}
+
+class CommentForm(forms.ModelForm):
+  class Meta:
+    model = Comment
+    fields = ['content']
+    labels = {'content':'댓글내용'}
     
