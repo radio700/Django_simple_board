e76590cd6a0d662f276c5eb779dfea2746bf9f7a
jaws <order18291@gmail.com>
2021-10-27 21:38:32 +0900

    ipchange

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k4a&5l+vkn17%h1(jpt3h*6(@*ah1ge9%i^w)ef@fjnl5e_6(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.35.65.164']
+ALLOWED_HOSTS = ['15.165.237.10']
 
 
 # Application definition
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.35.65.164', 'dev-poly-portfolio.link']
+ALLOWED_HOSTS = ['15.165.237.10']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
