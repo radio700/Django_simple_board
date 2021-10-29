b82676ff1ab7c199ca1b727a3ce79928821ce96e
jaws <order18291@gmail.com>
2021-10-30 02:57:40 +0900

    dns

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.36.176.49']
+ALLOWED_HOSTS = ['3.36.176.49','dev-poly-portfolio.link']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
