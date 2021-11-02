0fd71456fc021aacdb77d5c0cbc7ece6a2747c48
jaws <order18291@gmail.com>
2021-11-02 10:53:44 +0900

    start

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.35.40.27','dev-poly-portfolio.link']
+ALLOWED_HOSTS = ['3.35.40.27']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
