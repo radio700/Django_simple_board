7a066d929e180ad2109d689f45473b2ff795851e
jaws <order18291@gmail.com>
2021-11-11 18:37:34 +0900

    키오스크_프론트_30%_완성

--- kiosk/models.py
+++ kiosk/models.py
@@ -5,18 +5,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Recipe(models.Model):
-  Menucode = models.IntegerField(null=False)
-  Menu_kor = models.CharField(max_length=10, null=False)
-  Price = models.IntegerField(null=False)
+  menucode = models.IntegerField(null=False)
+  menu_name = models.CharField(max_length=10, null=False)
+  price = models.IntegerField(null=False)
 
   def __str__(self):
-      return self.Menu_kor
+      return self.menu_name
 
 class Jumun(models.Model):
   
-  Menucode = models.ForeignKey(Recipe, on_delete=models.CASCADE,null=False)
-  Count = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)], null=False)
-  Customer = models.ForeignKey(User,on_delete=models.CASCADE, null=False)
-  Jumun_date = models.DateTimeField(null=False)
-  Today_jumun_order = models.IntegerField(null=False)
+  menucode = models.ForeignKey(Recipe, on_delete=models.CASCADE,null=False)
+  count = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)], null=False)
+  customer = models.ForeignKey(User,on_delete=models.CASCADE, null=False)
+  jumun_date = models.DateTimeField(null=False)
+  today_jumun_order = models.IntegerField(null=False)
 
--- kiosk/views.py
+++ kiosk/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from kiosk.models import Recipe, Jumun
 # Create your views here.
 
 def index(response):
   return render(response,'kiosk/main.html')
 
 def menu(response):
-  return render(response,'kiosk/menu.html')
+  menu_list = Recipe.objects.order_by()
+  context = {'menu_list':menu_list}
+  return render(response,'kiosk/menu.html',context)
